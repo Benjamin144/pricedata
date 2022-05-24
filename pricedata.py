d11eb98dcffa56b938c38e0e8e7c204f5b4455f1
Benjamin144 <josephroberts@email.com>
2022-05-24 01:11:37 +0000

    formatting rendered data in terminal window

--- pricedata.py
+++ pricedata.py
@@ -3,16 +3,26 @@ import requests
 
 # simulates data from a website that sells PC systems and components
 # search for Laptops with specific specifications and their latest prices
-# my goal is to collect data on Lap tops that are VR Ready
-# Differentiating between Laptops that are not VR Ready
+
 
 # request is requsting infomation from the website:
-# https://www.overclockers.co.uk/gaming-laptops
+# https://www.cyberpowersystem.co.uk/category/laptops/
+# Targeting what looks like cards elements to home in on
+# info needed to identify product data
+# rendering this program in the terminal window
+# using a series of formatting to prettify the data results
 
 
 html_text = requests.get(
     'https://www.cyberpowersystem.co.uk/category/laptops/').text
 soup = BeautifulSoup(html_text, 'lxml')
+syst_name = soup.find('h2', class_='g-sys-name').text
+syst_spec = soup.find('ul', class_='g-sys-spec').text.replace(' ', '')
+syst_price = soup.find('p', class_='g-sys-price').text.replace(' ', '')
+print(f'''
+System Name: {syst_name}
+System Specification: {syst_spec}
+System Price: {syst_price}
+''')
 
-# Targeting what looks like cards elements - use a method that will target all
-# of the laptop posts by catching an element and inspecting them
+# syst = soup.find('ul', class_='g-sys-spec')
